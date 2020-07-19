4d522a1d7ec39f5350d8316b0b46c30948a1e39e
Paulina <paulinadesign@users.noreply.github.com>
2020-07-19 12:19:06 +0300

    Getting there

--- app/app.js
+++ app/app.js
@@ -1,14 +1,18 @@
 'use strict';
 
 // Declare app level module which depends on views, and core components
-angular.module('myApp', [
+angular.module("WeatherApp", [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+  //'myApp.view1',
+  //'myApp.view2',
   'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+]);
+
+
+
+
+/*config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}]);*/
