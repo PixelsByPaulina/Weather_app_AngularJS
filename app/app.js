5616c8aa9848307acb3424a1d8fbbf23efbb0150
Paulina <paulinadesign@users.noreply.github.com>
2020-07-19 22:17:15 +0300

    Perskaito API linko info!
    Linkas keiciasi pagal irasyta miesta.

--- app/app.js
+++ app/app.js
@@ -1,51 +1,52 @@
 "use strict";
 
 angular.module("weatherApp", [])
-    .controller("weatherController", function($scope) {
+    .controller("weatherController", function($scope, $http) {
         // CITY SELECTION
         $scope.writtenCity = "";
-        $scope.selectedCity = "";
+        $scope.selectedCity = "Vilnius";
+        $scope.currentUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Kaunas&appid=61831ea54b831b0ea5482f37d73f171d";
 
         var url = "https://api.openweathermap.org/data/2.5/forecast?";
-        var appId = "&appid=61831ea54b831b0ea5482f37d73f171d";
+        var APIkey = "&appid=61831ea54b831b0ea5482f37d73f171d";
 
+        // CREATE API LINK WITH BY CITY NAME
         $scope.findCity = function() {
-            $scope.selectedCity = "q=" + $scope.writtenCity;
-            $scope.currentUrl = url + $scope.selectedCity + appId;
+            // IF INPUT FIELD IS NOT EMPTY, ACCEPT THE VALUE
+            if ($scope.writtenCity !== "") {
+                $scope.selectedCity = "q=" + $scope.writtenCity;
+                $scope.currentUrl = url + $scope.selectedCity + APIkey;
+            }
+            $scope.getWeather();
         };
 
+        // CREATE API LINK WITH GEOLOCATION
         $scope.findAutomatic = function() {
-            $scope.selectedCity = "Radau";
-            $scope.currentUrl = url + $scope.selectedCity + appId;
+            $scope.selectedCity = "insert-Geolocation-info";
+            // lat=54.6892&lon=25.2798 <--toks formatas reikalingas
+            $scope.currentUrl = url + $scope.selectedCity + APIkey;
+            $scope.getWeather();
         };
 
 
-
-        //$scope.currentUrl = url + $scope.selectedCity + appId;
-
-
-        // FINDING CURRENT WEATHER BY CITY NAME
-        /*var getWeather = function(city) {
-          var url = "https://api.openweathermap.org/data/2.5/forecast?";
-            var cityName = "q=" + $scope.selectedCity;
-            var cityCoord = "lat=54.6892&lon=25.2798";
-            var appId = "&appid=61831ea54b831b0ea5482f37d73f171d";
+        $scope.getWeather = function() {
+            $http({
+                method: "GET",
+                url: $scope.currentUrl
+            })
+                .then(function (data, status) {
+                    if(data.data.cod !== "200") {
+                        $scope.temperature = "Miestas nerastas";
+                    }
+                    else {
+                        $scope.temperature = data.data;
+                    }
+                })
+                .error(function (data, status) {
+                    $scope.temperature = "Error";
+                });
         };
 
-        $geolocation.getLocation = function(position) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var latitude = position.coords.latitude;
-                var longitude = position.coords.longitude;
-
-                $http.json("https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&\n" +
-                    "exclude=minutely,hourly&appid=61831ea54b831b0ea5482f37d73f171d")
-                    .success(function(data) {
-                        $scope.weatherData = data;
-                        console.log(data);
-
-                    });
-            };
-        };*/
 
 
 
